23b96c4920ec4f379908af1be239354482eb6b7b
davidski <davidski@deadheaven.com>
2014-08-26 15:58:02 -0700

    module renames

--- vulnpryer.py
+++ vulnpryer.py
@@ -3,11 +3,12 @@
 import argparse
 from datetime import date, timedelta
 from dateutil.parser import parse
+import logging
 
 # VulnDB components
 from vulndb import query_vulndb
-from mongo_loader import load_mongo, get_extract
-from trl import get_trl, modify_trl, post_trl
+from stower import load_mongo, get_extract
+from forklift import get_trl, modify_trl, post_trl
 
 # set default dates
 to_date = date.today()
@@ -26,14 +27,21 @@ parser.add_argument('-e', '--enddate', type=mkdate, default=to_date,
                     help="Start date.")
 parser.add_argument('-s', '--startdate', type=mkdate, default=from_date,
                     help="End date.")
+parser.add_argument('-l', '--loglevel', default="info", help="Log level.")
 args = parser.parse_args()
 
-startstring = args.startdate
-startstring = startstring.strftime("%Y-%m-%d")
-endstring = args.enddate
-endstring = endstring.strftime("%Y-%m-%d")
+start_string = args.startdate
+start_string = start_string.strftime("%Y-%m-%d")
+end_string = args.enddate
+end_string = endstring.strftime("%Y-%m-%d")
 
-print "Range requested %s - %s" % (startstring, endstring)
+# set logging level
+numeric_level = getattr(logging, loglevel.upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueErrr('Invalid log level; %s' % loglevel)
+logging.basicConfig(level=numeric_level)
+
+print "Range requested %s - %s" % (start_string, end_string)
 query_vulndb(args.startdate, args.enddate)
 
 print "Loading data into Mongo"
