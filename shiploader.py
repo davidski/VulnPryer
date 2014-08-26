19bd04b60c8a821418eabe308daa57c1cf756101
davidski <davidski@deadheaven.com>
2014-08-26 16:47:18 -0700

    make headers dynamic and flip execute bit

--- shiploader.py
+++ shiploader.py
@@ -182,9 +182,10 @@ def _write_vulndb(results, filename):
     """Dump output to CSV"""
     csvfile = open(filename, 'wb')
 
-    headers = ['CVE_ID', 'OSVDB', 'public_exploit', 'private_exploit',
-        'cvss_score', 'msp', 'edp', 'network_vector', 'impact_integrity',
-        'impact_confidentialit', 'network_vector']
+    #headers = ['CVE_ID', 'OSVDB', 'public_exploit', 'private_exploit',
+    #    'cvss_score', 'msp', 'edb', 'network_vector', 'impact_integrity',
+    #    'impact_confidentialit', 'network_vector']
+    headers = results['result'][0].keys
     csvwriter = csv.DictWriter(csvfile, fieldnames=headers)
     csvwriter.writeheader()
     for result in results['result']:
