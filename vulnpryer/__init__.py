dbe0da34b83125e454f18aa153b79f600c0d7829
David F. Severski <davidski@deadheaven.com>
2016-12-23 09:11:45 -0800

    Lint the init file

--- vulnpryer/__init__.py
+++ vulnpryer/__init__.py
@@ -2,5 +2,5 @@ from pkg_resources import get_distribution, DistributionNotFound
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
-   # package is not installed
-   pass
+    # package is not installed
+    pass
