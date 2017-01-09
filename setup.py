2a63d0ce1d0b077267fca389fd32210f1e0509c8
David F. Severski <davidski@deadheaven.com>
2017-01-08 20:08:28 -0800

    Allow setup to deploy conf file into an arbitrary location

--- setup.py
+++ setup.py
@@ -1,4 +1,40 @@
 from setuptools import setup
+from setuptools.command.install import install
+import warnings
+
+
+# Robust datafile deployment process courtesy
+# http://stackoverflow.com/questions/34193900/how-do-i-distribute-fonts-with-my-python-package
+class MoveConf(install):
+    def run(self):
+        """
+        Performs the usual install process and then copies the sample conf
+        into the etc directory
+        """
+        # perform the standard install process
+        install.run(self)
+        # try to deploy our configuration sample
+        try:
+            import os
+            import shutil
+            import vulnpryer as vp
+
+            # Find where to store the conf file
+            target_dir = os.path.dirname('etc')
+
+            # Copy the sample config
+            sample_conf = os.path.join(os.path.dirname(vp.__file__), 'conf')
+            for file_name in os.listdir(sample_conf):
+                if file_name[-5:] == '.conf':
+                    old_path = os.path.join(sample_conf, file_name)
+                    new_path = os.path.join(target_dir, file_name)
+                    shutil.copyfile(old_path, new_path)
+                    print("Copying {} -> {}".format(old_path,  new_path))
+
+        except Exception:
+            warnings.warn("WARNING: Unspecified issue deploying " +
+                          "sample configuration file.")
+
 
 setup(
     name='VulnPryer',
@@ -17,26 +53,29 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Security'
     ],
     install_requires=[
+        "configparser >= 2.5.0",
         "boto3 >= 1.4.0",
         "filechunkio >= 1.8.0",
+        "future >= 0.16.0",
         "lxml >= 3.7.1",
         "pymongo[tls] >= 3.4.0",
-        "simplejson >= 3.10.0",
+        "python-crontab >= 2.1.1",
+        "python-dateutil >= 2.6.0",
         "requests >= 2.12.0",
         "requests_oauthlib >= 0.7.0",
-        "python-dateutil >= 2.6.0",
-        "configparser >= 2.5.0",
-        "future >= 0.16.0",
-        "python-crontab >= 2.1.1"
+        "simplejson >= 3.10.0"
     ],
     scripts=['bin/vulnpryer'],
     include_package_data=True,
     keywords='security vulnerability vulndb redseal',
-    data_files=[('/etc', ['conf/vulnpryer.conf.sample'])],
+    data_files=[('', ['conf/vulnpryer.conf.sample'])],
+    cmdclass={'install': MoveConf}
 )
