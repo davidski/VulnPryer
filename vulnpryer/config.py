27b83b843670c57f65256fb02227dfed76154c33
David F. Severski <davidski@deadheaven.com>
2017-01-08 20:09:10 -0800

    Config can be retrieved from /etc /usr/local/etc, cwd, or ~

--- vulnpryer/config.py
+++ vulnpryer/config.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from configparser import ConfigParser
+import os
 
 
 def read_config(conf_file='/etc/vulnpryer.conf'):
     """Read the config file and return a config object"""
     config = ConfigParser()
-    config.read(conf_file)
+    config.read([conf_file, '/usr/local/etc/vulnpryer.conf',
+                 os.path.expanduser('~/vulnpryer.conf')])
     return config
